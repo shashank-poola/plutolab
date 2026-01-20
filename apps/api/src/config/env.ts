219633cac5714bdb9a76d2eb5bd285e2893b86cd
shashank-poola <shashankpoola123@gmail.com>
2026-01-20 22:26:03 +0530

    feat(api): config/env.ts zod schema for .env file

---
+++ apps/api/src/config/env.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import path from "path";
+import { z } from "zod";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../.env"),
+});
+
+const EnvSchema = z.object({
+  SERVER_PORT: z.string().default("8000").transform(Number),
+  JWT_SECRET: z.string().min(1),
+  SERVER_GOOGLE_GEMINI_API: z.string().optional(),
+  SERVER_GROQ_API: z.string().optional(),
+  NEXT_PUBLIC_APP_URL: z.string().optional(),
+});
+
+export const env = EnvSchema.parse(process.env);
