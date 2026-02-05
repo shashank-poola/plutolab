1df542b724c235e989f598d25edf4b6b698a453f
shashank-poola <shashankpoola123@gmail.com>
2026-02-06 01:43:46 +0530

    chron(backend): chat-controller -> project controller

--- apps/api/src/routes/projectRoute.ts
+++ apps/api/src/routes/projectRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getPlayground, createPlayground } from "../controller/chat-controller/createPlayground";
+import { getPlayground, createPlayground } from "../controller/project-controller/createPlayground";
 import authMiddleware from "../middleware/authMiddleware";
 
 const projectRouter = Router();
+
+projectRouter.use(authMiddleware);
 
 projectRouter.post("/", createPlayground);
 projectRouter.get("/", getPlayground);
