33ac4cbee2fafc7a4822465951d93cf20f93f389
shashank-poola <shashankpoola123@gmail.com>
2026-02-14 00:30:46 +0530

    feat(api): create project

--- apps/api/src/controller/chat-controller/startChat.ts
+++ apps/api/src/controller/chat-controller/startChat.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
 export const startPlaygroundChat = async ( req: Request, res: Response ) => {
-
+    
 };
--- apps/api/src/controller/project-controller/createPlayground.ts
+++ apps/api/src/controller/project-controller/createPlayground.ts
@@ -1,7 +1,16 @@
 import type { Request, Response } from "express";
 
 export const createPlayground = async ( req: Request, res: Response ) => {
+    try {
 
+    } catch (error) {
+        res.status(500).json({
+            "success": false,
+            "data": null,
+            "error": "INTERNAL_SERVER_ERROR"
+        })
+        return;
+    }
 };
 
 export const getPlayground = async ( req: Request, res: Response ) => {
